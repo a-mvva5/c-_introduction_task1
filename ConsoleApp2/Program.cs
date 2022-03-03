f4298516c305122ed3d16a1919bdc153b3a1e78e
Aysu Memmedova <aysu.memmedova2270@uni.edu.az>
2022-03-04 01:29:42 +0400

    a

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ilk ededi daxil edin :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Son ededi daxil edin :");
+            int m = Convert.ToInt32(Console.ReadLine()); ;
+            int sum = 0;
+            while (n<m)
+            {
+                if (n% 7 == 0)
+                {
+                    sum++;
+                }
+                n++;
+            }
+            Console.Write("Ilk ve son ededleri arasindaki 7 -e bolunen ededlerin sayi:  ");
+            Console.Write(sum);
+        }
+    }
+}
